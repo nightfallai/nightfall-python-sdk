bac1803ca6d11bda63b660903b2c90a36a5d10d3
Lev Lazinskiy <lev@levlaz.org>
2021-07-26 02:13:03 +0000

    Prepare 0.6.0 release

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -28,7 +28,7 @@ author = 'Nightfall'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc'
+    'sphinx.ext.autodoc', 'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
