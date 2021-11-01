766f5eb9508f6a57ec5a42f4b00fb686d05f8e40
Dan Hertz <dan@nightfall.ai>
2021-11-01 16:24:38 -0700

    update to 1.0.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
     name="nightfall",
-    version="1.0.0",
+    version="1.0.1",
     description="Python SDK for Nightfall",
     long_description=readme(),
     long_description_content_type="text/markdown",
