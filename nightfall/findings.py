b4ebc1c3de7a94fc00fbe90437442246d1d1e62c
Galen O'Sullivan <galen@nightfall.ai>
2022-02-23 10:05:57 -0500

    Add row_range and column_range
    * Update tests to use row and column range
    * Mark more fields of findings as Optional

--- nightfall/findings.py
+++ nightfall/findings.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional, Any
 from dataclasses import dataclass
 
 from nightfall.detection_rules import Confidence
@@ -31,6 +31,8 @@ class Finding:
         byte_range (Range): The byte range in which a finding was detected within the item.
         codepoint_range (Range): The codepoint range in which a finding was detected within the item. This differs
             from byte range since a codepoint may contain multiple bytes.
+        row_range (Range): The row in which a finding was detected, if it was in a tabular document. Index starts at 1.
+        column_range (Range): The column(s) in which a finding was detected, if it was in a tabular document. Index starts at 1.
         commit_hash (str): The hash of the commit in which the finding was detected, if known.
         matched_detection_rule_uuids (List[str]): The list of detection rule UUIDs that contained a detector that
             triggered a match.
@@ -38,14 +40,16 @@ class Finding:
             a match.
     """
     finding: str
-    redacted_finding: str
-    before_context: str
-    after_context: str
-    detector_name: str
+    redacted_finding: Optional[str]
+    before_context: Optional[str]
+    after_context: Optional[str]
+    detector_name: Optional[str]
     detector_uuid: str
     confidence: Confidence
     byte_range: Range
     codepoint_range: Range
+    row_range: Optional[Range]
+    column_range: Optional[Range]
     commit_hash: str
     matched_detection_rule_uuids: List[str]
     matched_detection_rules: List[str]
@@ -62,7 +66,18 @@ class Finding:
             Confidence[resp["confidence"]],
             Range(resp["location"]["byteRange"]["start"], resp["location"]["byteRange"]["end"]),
             Range(resp["location"]["codepointRange"]["start"], resp["location"]["codepointRange"]["end"]),
+            _range_or_none(resp["location"]["rowRange"]),
+            _range_or_none(resp["location"]["columnRange"]),
             resp["location"].get("commitHash", ""),
             resp["matchedDetectionRuleUUIDs"],
             resp["matchedDetectionRules"]
         )
+
+def _range_or_none(range_or_none: Any) -> Optional[Range]:
+    """Some ranges are not always present, this function returns either None or a Range."""
+    if range_or_none is None:
+        return None
+    start = range_or_none["start"]
+    end = range_or_none["end"]
+    return Range(start, end)
+
